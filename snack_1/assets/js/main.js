d2c91047fa07f2606f2a60a382b41ef3fdaca598
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-24 15:55:39 +0200

    snack 1

--- snack_1/assets/js/main.js
+++ snack_1/assets/js/main.js
@@ -8,53 +8,66 @@ const zucchine = [
     {
         varietà: 'verde',
         lunghezza: '10cm',
-        peso: '1'
+        peso: 1
     },
     {
         varietà: 'chiara',
         lunghezza: '5cm',
-        peso: '2'
+        peso: 2
     },
     {
         varietà: 'tonda',
         lunghezza: '6cm',
-        peso: '4'
+        peso: 4
     },
     {
         varietà: 'fiorentina',
         lunghezza: '8cm',
-        peso: '1'
+        peso: 1
     },
     {
         varietà: 'trombetta',
         lunghezza: '3cm',
-        peso: '3'
+        peso: 3
     },
     {
         varietà: 'gialla',
         lunghezza: '4cm',
-        peso: '5'
+        peso: 5
     },
     {
         varietà: 'siciliana',
         lunghezza: '7cm',
-        peso: '8'
+        peso: 8
     },
     {
         varietà: 'triestina',
         lunghezza: '12cm',
-        peso: '2'
+        peso: 2
     },
     {
         varietà: 'crookneck',
         lunghezza: '13cm',
-        peso: '6'
+        peso: 6
     },
     {
         varietà: 'pâtisson',
         lunghezza: '16cm',
-        peso: '5'
+        peso: 5
     },
 ]
 
 console.log(zucchine);
+
+
+let sum = 0
+
+zucchine.forEach(zucchina => {
+sum += zucchina.peso
+}) 
+console.log(sum);
+
+
+
+
+
